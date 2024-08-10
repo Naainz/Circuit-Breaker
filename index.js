45b65c9a6053164772d43b1ca069ecef709dab65
Stefan <67121265+Naainz@users.noreply.github.com>
2024-08-10 21:57:58 +0300

    Circuit Breaker pt 1.

--- index.js
+++ index.js
@@ -1,54 +1,138 @@
 /*
 First time? Check out the tutorial game:
 https://sprig.hackclub.com/gallery/getting_started
-
-@title: 
-@author: 
+@title: Circuit Breaker
+@author: Stefan D
 @tags: []
-@addedOn: 2024-00-00
+@addedOn: 2024-10-08
 */
 
-const player = "p"
+const player = "p";
+const wall = "w";
+const wire = "1";
+const wireActive = "2";
+const switchOff = "s";
+const switchOn = "S";
 
 setLegend(
-  [ player, bitmap`
+  [player, bitmap`
+  . . . . . . . .
+  . . 5 5 5 5 . .
+  . 5 . . . . 5 .
+  . 5 . 5 5 . 5 .
+  . 5 . 5 5 . 5 .
+  . 5 . . . . 5 .
+  . . 5 5 5 5 . .
+  . . . . . . . .
+  `],
+  [wall, bitmap`
+................
+..3.3.3.3.3.3...
+..3.3.3.3.3.3...
+..3.3.3.3.3.3...
+..3.3.3.3.3.3...
+..3.3.3.3.3.3...
+..3.3.3.3.3.3...
 ................
 ................
-.......000......
-.......0.0......
-......0..0......
-......0...0.0...
-....0003.30.0...
-....0.0...000...
-....0.05550.....
-......0...0.....
-.....0....0.....
-.....0...0......
-......000.......
-......0.0.......
-.....00.00......
-................` ]
-)
+................
+................
+................
+................
+................
+................
+................`],
+  [wire, bitmap`
+  . . . . . . . .
+  . . . . . . . .
+  . . . 2 2 . . .
+  . . . 2 2 . . .
+  . . . 2 2 . . .
+  . . . 2 2 . . .
+  . . . . . . . .
+  . . . . . . . .
+  `],
+  [wireActive, bitmap`
+  . . . . . . . .
+  . . . . . . . .
+  . . . 6 6 . . .
+  . . . 6 6 . . .
+  . . . 6 6 . . .
+  . . . 6 6 . . .
+  . . . . . . . .
+  . . . . . . . .
+  `],
+  [switchOff, bitmap`
+  . . . . . . . .
+  . . 4 4 4 4 . .
+  . 4 . . . . 4 .
+  . 4 . 4 4 . 4 .
+  . 4 . 4 4 . 4 .
+  . 4 . . . . 4 .
+  . . 4 4 4 4 . .
+  . . . . . . . .
+  `],
+  [switchOn, bitmap`
+  . . . . . . . .
+  . . 8 8 8 8 . .
+  . 8 . . . . 8 .
+  . 8 . 8 8 . 8 .
+  . 8 . 8 8 . 8 .
+  . 8 . . . . 8 .
+  . . 8 8 8 8 . .
+  . . . . . . . .
+  `]
+);
 
-setSolids([])
+const level = map`
+wwwwwwwwww
+w........w
+w.p......w
+w..1..s..w
+w..1..w..w
+w..1..w..w
+w..1..w..w
+wwwwwwwwww
+`;
 
-let level = 0
-const levels = [
-  map`
-p.
-..`
-]
+setMap(level);
+setSolids([player, wall]);
 
-setMap(levels[level])
+onInput("w", () => {
+  const p = getFirst(player);
+  if (p) p.y -= 1;
+});
 
-setPushables({
-  [ player ]: []
-})
+onInput("a", () => {
+  const p = getFirst(player);
+  if (p) p.x -= 1;
+});
 
 onInput("s", () => {
-  getFirst(player).y += 1
-})
+  const p = getFirst(player);
+  if (p) p.y += 1;
+});
+
+onInput("d", () => {
+  const p = getFirst(player);
+  if (p) p.x += 1;
+});
 
 afterInput(() => {
+  const p = getFirst(player);
+  if (p) {
+    const playerTile = getTile(p.x, p.y);
   
-})
+    if (playerTile.find(tile => tile.type === switchOff)) {
+      clearTile(p.x, p.y);
+      addSprite(p.x, p.y, switchOn);
+      
+      getAll(wire).forEach(wireTile => {
+        clearTile(wireTile.x, wireTile.y);
+        addSprite(wireTile.x, wireTile.y, wireActive);
+      });
+
+      addText("Circuit Activated!", { x: 1, y: 1, color: color`5` });
+    }
+  }
+});
