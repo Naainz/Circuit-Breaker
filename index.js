4989d3009f0e2e63c75e48a443483ce6000401e9
Stefan <67121265+Naainz@users.noreply.github.com>
2024-08-11 13:03:43 +0300

    Update index.js

--- index.js
+++ index.js
@@ -15,10 +15,18 @@ const switchOff = "s";
 const switchOn = "S";
 const ghost = "g";
 
-let ghost1XPosition = 2;
-let ghost1YPosition = 3;
-let ghost2XPosition = 7;
-let ghost2YPosition = 5;
+let ghost1XPosition = 5;
+let ghost1YPosition = 4;
+let ghost2XPosition = 2;
+let ghost2YPosition = 3;
+let ghost3XPosition = 7;
+let ghost3YPosition = 5;
+
+let ghost1XDirection = 0;
+let ghost1YDirection = 1;
+let ghost2XDirection = 1;
+let ghost2YDirection = 1;
+let ghost3Direction = 1;
 
 setLegend(
   [player, bitmap`
@@ -118,9 +126,7 @@ wwwwwwwwww
 let maxSteps = 6;
 let currentSteps = 0;
 let isGameOver = false;
-let ghost1Direction = 1;
-let ghost2Direction = 1;
-let ghost1Interval, ghost2Interval;
+let ghost1Interval, ghost2XInterval, ghost2YInterval, ghost3Interval;
 
 function updateStepCounter() {
   clearText();
@@ -134,21 +140,24 @@ function resetGame() {
   isGameOver = false;
   updateStepCounter();
   clearText();
-  startGhost1Movement();
-  startGhost2Movement();
+  startLevel1GhostMovement(); 
 }
 
 function gameOver(message) {
   isGameOver = true;
   clearInterval(ghost1Interval);
-  clearInterval(ghost2Interval);
+  clearInterval(ghost2XInterval);
+  clearInterval(ghost2YInterval);
+  clearInterval(ghost3Interval);
   addText(message, { x: 1, y: 8, color: color`0` });
   addText("Press W!", { x: 1, y: 9, color: color`0` });
 }
 
 function moveToLevel2() {
   clearInterval(ghost1Interval);
-  clearInterval(ghost2Interval);
+  clearInterval(ghost2XInterval);
+  clearInterval(ghost2YInterval);
+  clearInterval(ghost3Interval);
   addText("Circuit Achieved!", { x: 1, y: 8, color: color`0` });
   setTimeout(() => {
     setMap(level2);
@@ -156,8 +165,8 @@ function moveToLevel2() {
     currentSteps = 0;
     isGameOver = false;
     clearText();
-    startGhost1Movement();
-    startGhost2Movement();
+    startGhost2MovementLevel2();
+    startGhost3MovementLevel2();
     updateStepCounter();
   }, 1000);
 }
@@ -231,43 +240,82 @@ function checkGameOver() {
   }
 }
 
-function startGhost1Movement() {
+function startLevel1GhostMovement() {
   ghost1Interval = setInterval(() => {
     if (isGameOver) return;
     const g1 = getAll(ghost).find(g => g.x === ghost1XPosition && g.y === ghost1YPosition);
     if (g1) {
-      const newY = g1.y + ghost1Direction;
+      let newY = ghost1YPosition + ghost1YDirection;
       if (newY < 1 || newY > 6) {
-        ghost1Direction *= -1;
+        ghost1YDirection *= -1;
+        newY = ghost1YPosition + ghost1YDirection;
       }
-      g1.y += ghost1Direction;
+      ghost1YPosition = newY;
+      g1.y = newY;
 
-      const playerTile = getTile(g1.x, g1.y);
-      if (playerTile.some(tile => tile.type === player)) {
+      if (getTile(g1.x, g1.y).some(tile => tile.type === player)) {
         gameOver("You were caught!");
       }
     }
-  }, 200);
+  }, 2000);
 }
 
-function startGhost2Movement() {
-  ghost2Interval = setInterval(() => {
+function startGhost2MovementLevel2() {
+  ghost2XInterval = setInterval(() => {
     if (isGameOver) return;
     const g2 = getAll(ghost).find(g => g.x === ghost2XPosition && g.y === ghost2YPosition);
     if (g2) {
-      const newX = g2.x + ghost2Direction;
+      let newX = ghost2XPosition + ghost2XDirection;
       if (newX < 1 || newX > 8) {
-        ghost2Direction *= -1;
+        ghost2XDirection *= -1;
+        newX = ghost2XPosition + ghost2XDirection;
       }
-      g2.x += ghost2Direction;
+      ghost2XPosition = newX;
+      g2.x = newX;
 
-      const playerTile = getTile(g2.x, g2.y);
-      if (playerTile.some(tile => tile.type === player)) {
+      if (getTile(g2.x, g2.y).some(tile => tile.type === player)) {
         gameOver("You were caught!");
       }
     }
-  }, 400);
+  }, 4000);
+
+  ghost2YInterval = setInterval(() => {
+    if (isGameOver) return;
+    const g2 = getAll(ghost).find(g => g.x === ghost2XPosition && g.y === ghost2YPosition);
+    if (g2) {
+      let newY = ghost2YPosition + ghost2YDirection;
+      if (newY < 1 || newY > 6) {
+        ghost2YDirection *= -1;
+        newY = ghost2YPosition + ghost2YDirection;
+      }
+      ghost2YPosition = newY;
+      g2.y = newY;
+
+      if (getTile(g2.x, g2.y).some(tile => tile.type === player)) {
+        gameOver("You were caught!");
+      }
+    }
+  }, 2000);
 }
 
-startGhost1Movement();
-startGhost2Movement();
+function startGhost3MovementLevel2() {
+  ghost3Interval = setInterval(() => {
+    if (isGameOver) return;
+    const g3 = getAll(ghost).find(g => g.x === ghost3XPosition && g.y === ghost3YPosition);
+    if (g3) {
+      let newY = ghost3YPosition + ghost3Direction;
+      if (newY < 1 || newY > 6) {
+        ghost3Direction *= -1;
+        newY = ghost3YPosition + ghost3Direction;
+      }
+      ghost3YPosition = newY;
+      g3.y = newY;
+
+      if (getTile(g3.x, g3.y).some(tile => tile.type === player)) {
+        gameOver("You were caught!");
+      }
+    }
+  }, 2000);
+}
+
+startLevel1GhostMovement();
